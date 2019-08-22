c5d3b7c1c1371aa362daf9a69313ef36a0e5eb5c
Dominick Bruno <dbruno@unseen.is>
2019-08-22 12:38:52 -0400

    Added grid styling to rendered test stories

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import StoryList from './components/storyList';
 
+import 'semantic-ui-css/semantic.min.css';
+
 function App() {
   return (
     <div className="App">
