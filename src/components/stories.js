224dab992a88d49b06540e6e0c729d1c669ad4f1
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-08-27 20:03:30 -0700

    Forms-are-working!

--- src/components/stories.js
+++ src/components/stories.js
@@ -31,44 +31,27 @@ const StoryForm = ({ errors, touched, values, status }) => {
       </h2>
       <Form>
         <h1 className="header"> Submit your Story below</h1>
-        <label className="text-container">First Name</label>
+        <label className="text-container">Full Name</label>
         <Field
           component="input"
           type="text"
-          name="firstname"
-          placeholder="First Name"
+          name="name"
+          placeholder="Full Name"
         />
 
-        {touched.firstname && errors.firstname && (
-          <p className="error">{errors.firstname}</p>
-        )}
-        <label className="text-container">Last Name</label>
-        <Field
-          component="input"
-          type="text"
-          name="lastname"
-          placeholder="Last Name"
-        />
-        {touched.lastname && errors.lastname && (
-          <p className="error">{errors.lastname}</p>
-        )}
-        <label className="text-container">Email</label>
-        <Field component="input" type="text" name="email" placeholder="Email" />
-        {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
         <label className="text-container">Photo</label>
-        <Field
+        {/* <Field
           component="input"
           type="text"
           name="photo"
           placeholder="Photo Title"
-        />
+        /> */}
         <button>Attach</button>
         {touched.photo && errors.photo && (
           <p className="error">{errors.photo}</p>
         )}
-        <label className="text-container">Title</label>
+        <label className="text-container">Story Title</label>
         <Field
           component="input"
           type="text"
@@ -89,18 +72,7 @@ const StoryForm = ({ errors, touched, values, status }) => {
           <p className="error">{errors.notes}</p>
         )}
 
-        {/* <label className="text-container">Password</label>
-        <Field type="password" name="password" placeholder="Password" />
-        {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
-        <label className="checkbox-container">
-          I would like to join the mailing list
-          <Field type="checkbox" name="mail" checked={values.terms} />
-          <span className="checkmark" />
-        </label> */}
-
-        <button>Submit!</button>
+        <button type="submit">Submit!</button>
 
         <NavLink to="/">
           <button>Back</button>
@@ -114,24 +86,25 @@ const StoryForm = ({ errors, touched, values, status }) => {
 };
 
 const formikHOC = withFormik({
-  mapPropsToValues({ firstname, lastname, email, photo, title, story }) {
+  mapPropsToValues({ name, photo, title, story }) {
     return {
-      firstname: firstname || "",
-      lastname: lastname || "",
-      email: email || "",
+      name: name || "",
       photo: photo || "",
       title: title || "",
       story: story || false
     };
   },
-  validationSchema: Yup.object().shape({
-    name: Yup.string().required("not a good input"),
-    email: Yup.number().required(),
-    password: Yup.string().required("please enter a password")
-  }),
+  // validationSchema: Yup.object().shape({
+  //   name: Yup.string().required("not a good input"),
+  //   email: Yup.number().required(),
+  //   password: Yup.string().required("please enter a password")
+  // }),
   handleSubmit(values, { setStatus, resetForm }) {
     axios
-      .post("https://refugee-stories-backend-bw.herokuapp.com/signup", values)
+      .post(
+        "https://refugee-stories-backend-bw.herokuapp.com/submitStory",
+        values
+      )
       .then(res => {
         console.log("handleSubmit: then: res: ", res);
         setStatus(res.data);
