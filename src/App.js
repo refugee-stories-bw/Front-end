0eefbb0188c4fde830e6c0cecac17f3408277e93
Dominick Bruno <dbruno@unseen.is>
2019-08-27 23:09:34 -0400

    Revert "LoginSignUp-and-Submit-Pages-styled"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import logo from "./logo.svg";
-import "./App.css";
-import AppRouter from "./components/AppRouter";
-// import LoginFormik from "./login";
-// import SignupForm from "./SignupForm"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 import StoryList from './components/storyList';
+
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
     <div className="App">
-      <AppRouter />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
